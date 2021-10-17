cbb1029b70d97ee101be8011d7d68fbf119d3d51
sagor9116 <sagor.roy9116@gmail.com>
2021-10-17 16:22:41 +0600

    portfolio & testimonial sections are completed

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+// owl-carousel
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true, 
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1,
+        },
+    },
+})
